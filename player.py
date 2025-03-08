3fe722929e92c7be045c1c6d4298f81b40d24e7b
ChantzD <daltonca31@gmail.com>
2025-03-08 09:22:24 -0500

    Added player movement

--- player.py
+++ player.py
@@ -6,6 +6,22 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        return
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_a]:
+            self.rotate(dt * -1)
+
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
+
+        if keys[pygame.K_d]:
+            self.rotate(dt)
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -17,3 +33,12 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        return
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        return
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
